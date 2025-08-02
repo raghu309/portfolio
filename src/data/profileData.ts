d3f4e24dbf18b0c8b14ba0a2da761e0669d37fc9
Raghu <ragavendrarag309@gmail.com>
2025-08-02 21:02:38 +0530

    Fixed image paths

--- src/data/profileData.ts
+++ src/data/profileData.ts
@@ -1,3 +1,4 @@
+
 type Profile = {
   name: string;
   link: string;
@@ -10,19 +11,19 @@ const profileData: Profile[] = [
     name: 'LeetCode',
     link: 'https://leetcode.com/u/raghu30/',
     description: 'Problem Solving & DSA',
-    image: '/assets/profiles/leetcode-ss.png',
+    image: '/portfolio/assets/profiles/leetcode-ss.png',
   },
   {
     name: 'Codeforces',
     link: 'https://codeforces.com/profile/raghu3',
     description: 'Competitive Programming',
-    image: '/assets/profiles/codeforces-ss.png'
+    image: '/portfolio/assets/profiles/codeforces-ss.png'
   },
   {
     name: 'Kaggle',
     link: 'https://www.kaggle.com/raghu33',
     description: 'Machine Learning',
-    image: '/assets/profiles/kaggle-ss.png'
+    image: '/portfolio/assets/profiles/kaggle-ss.png'
   },
 ];
 
--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -4,7 +4,7 @@ const projectData = [
         title: 'AI Email Assistant',
         description: 'Built a smart email assistant to help users draft replies efficiently. The frontend is developed with React, and the backend uses Spring Boot. Integrated Gemini API to generate AI-based email replies. Also created a Gmail extension that injects a custom reply button into the Gmail compose window, enabling users to generate responses directly within Gmail.',
         tags: ['React', 'Spring Boot', 'Tailwind CSS', 'Gemini API', 'Javascript', 'Google Extension'],
-        image: '/assets/projects/ai-mail-assistant-ss.png',
+        image: '/portfolio/assets/projects/ai-mail-assistant-ss.png',
         link: 'https://github.com/raghu309/ai-mail-assistant-and-extension',
     },
     {
@@ -12,7 +12,7 @@ const projectData = [
         title: 'React-Native Movie App',
         description: 'Developed a cross-platform mobile application using React Native and Expo to browse and search for movies. Integrated with a public movie API (e.g., TMDB) to display trending, upcoming, and top-rated films. Implemented smooth navigation, responsive UI, and dynamic search functionality for an intuitive user experience on both Android and iOS devices.',
         tags: ['React Native', 'Expo', 'TypeScript', 'Mobile App', 'TMDB API'],
-        image: '/assets/projects/react-native-movie-ss.png',
+        image: '/portfolio/assets/projects/react-native-movie-ss.png',
         link: 'https://github.com/raghu309/movie-native-app',
     },
     {
@@ -20,7 +20,7 @@ const projectData = [
         title: 'Blog App',
         description: 'Built a full-stack blogging platform with React frontend and Node.js backend, using MongoDB for data storage. Implemented features to add, edit, and delete blog posts. Integrated JWT-based authentication to enable secure user login and registration.',
         tags: ['React', 'Node.js', 'MongoDB', 'JWT', 'Express', 'Blog App'],
-        image: '/assets/projects/blog-ss.png',
+        image: '/portfolio/assets/projects/blog-ss.png',
         link: 'https://github.com/raghu309/blog-frontent',
     },
      {
@@ -28,7 +28,7 @@ const projectData = [
         title: 'Movie Mania',
         description: 'Built a responsive movie search application using React. Users can search for movies by title, with results fetched from TMDB API. Designed a clean and user-friendly interface for smooth browsing.',
         tags: ['React', 'JavaScript', 'Movie API', 'Responsive Design'],
-        image: '/assets/projects/movie-web-ss.png',
+        image: '/portfolio/assets/projects/movie-web-ss.png',
         link: 'https://github.com/raghu309/movie_mania',
     },
     {
@@ -36,7 +36,7 @@ const projectData = [
         title: 'ToDo App',
         description: 'Created a full-stack todo application with React for the frontend and Node.js for the backend. Used MongoDB with Mongoose to store and manage tasks. Implemented features to add, update, delete, and mark todos as completed, providing a smooth and responsive user experience.',
        tags: ['React', 'Node.js', 'MongoDB', 'Mongoose', 'Express', 'Todo App'],
-        image: '/assets/projects/todolist-ss.png',
+        image: '/portfolio/assets/projects/todolist-ss.png',
         link: 'https://github.com/raghu309/ToDoList',
     },
     {
@@ -44,7 +44,7 @@ const projectData = [
         title: 'Bank Churn Detection',
         description: 'Built a machine learning model to predict customer churn using a Random Forest classifier. Performed exploratory data analysis to identify key patterns, and applied preprocessing techniques such as one-hot encoding, feature scaling, and missing value handling. Achieved effective classification performance using well-tuned hyperparameters.',
         tags: ['Python', 'Machine Learning', 'Random Forest', 'Pandas', 'Scikit-learn', 'EDA'],
-        image: '/assets/projects/bank-churn-ss.png',
+        image: '/portfolio/assets/projects/bank-churn-ss.png',
         link: 'https://github.com/raghu309/BankChurnDataset',
     },
     {
@@ -52,7 +52,7 @@ const projectData = [
         title: 'Detect AI Generated Text',
         description: 'Developed a machine learning model to identify AI-generated text using tokenization for text preprocessing and a Random Forest classifier for prediction. Processed textual data to extract meaningful features and trained the model to distinguish between human-written and AI-generated content with high accuracy.',
         tags: ['Python', 'Machine Learning', 'NLP', 'Tokenization', 'Random Forest', 'Scikit-learn'],
-        image: '/assets/projects/detect-ai-gen-text-ss.png',
+        image: '/portfolio/assets/projects/detect-ai-gen-text-ss.png',
         link: 'https://github.com/raghu309/LLM---Detect-AI-Generated-Text',
     }
 ];
